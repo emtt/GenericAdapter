73d449f12d44f18758e40a80408cb58fe0976076
emtt <emoralest@gmail.com>
2019-04-24 19:13:10 -0500

    Passing the view and position of element clicked to the Activity

--- genericadapter/src/main/java/mobilize/mx/genericadapter/ItemListener.java
+++ genericadapter/src/main/java/mobilize/mx/genericadapter/ItemListener.java
@@ -1,9 +1,11 @@
 package mobilize.mx.genericadapter;
 
+import android.view.View;
+
 /**
  * Callback for clicks on AppAdapter's item
  * @param <T>
  */
 public interface ItemListener<T> {
-    void onClick(T model);
+    void onClick(T model, View v, int position);
 }
